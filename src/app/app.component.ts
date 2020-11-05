33a36230c9c9281ba32db10d890e35772f1b3599
farhad <farhad.surani@gmail.com>
2020-11-05 10:33:57 -0600

    #new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,101 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import {  Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { LoginPage } from '../pages/login/login';
+import { WeatherPage } from '../pages/weather/weather';
+import { ConfigUrls } from '../config/config';
+import { AppServiceProvider } from '../providers/app-service/app-service';
+import { CumulativeSnowfallPage } from '../pages/cumulative-snowfall/cumulative-snowfall';
+import { cardPage } from '../pages/card/card';
+import { aboutUSPage } from '../pages/aboutUS/aboutUS';
+import { OneSignal } from '@ionic-native/onesignal';
+import { timer } from 'rxjs/observable/timer';
+import { Http } from '@angular/http';
+//import { InAppPurchase } from '@ionic-native/in-app-purchase';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+
   @ViewChild(Nav) nav: Nav;
+  rootPage: any;
+  activePage: any;
+ first =0;
 
-  rootPage: any = HomePage;
-
-  pages: Array<{title: string, component: any}>;
-
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  constructor(private OneSignal: OneSignal,public platform: Platform, public statusBar: StatusBar,
+  public splashScreen: SplashScreen, public appServiceProvider : AppServiceProvider,public oneSignal: OneSignal,public http:Http) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
-    ];
-
+    //this.statusBar.hide();
+    
+    //this.splashScreen.hide();
+   
+    
   }
+
+
 
   initializeApp() {
+
+   
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+
+      // setTimeout(() => {
+              
+              this.statusBar.overlaysWebView(false);
+              this.statusBar.backgroundColorByHexString('#000000');
+              //this.statusBar.
+               
+              
+             //timer(3000).subscribe(() => this.showSplash = false)
+               this.appServiceProvider.getAuthkey().then(token => {
+                                        console.log(token);
+                                        
+                                        if(token == '')
+                                        this.rootPage = LoginPage;
+                                  else{
+                                        ConfigUrls.authtoken = token;
+                                        this.rootPage = WeatherPage;
+                                  }
+                                  setTimeout(() => {
+                                    this.splashScreen.hide();
+                                  },3000);
+
+ // Here you places data we collect in step 0:07 
+ this.oneSignal.startInit('ffc0da08-6c3a-43d8-a7dd-2b1eb72f6c82');
+
+ this.oneSignal.inFocusDisplaying(this.oneSignal.OSInFocusDisplayOption.InAppAlert);
+ 
+ this.oneSignal.handleNotificationReceived().subscribe(() => {
+  // do something when notification is received
+
+  
+ });
+ 
+ this.oneSignal.handleNotificationOpened().subscribe(() => {
+   // do something when a notification is opened
+ 
+ });
+ 
+ this.oneSignal.endInit();
+
+                                     
+               });
+      // }, 100);
     });
+
+    
+
   }
+
+  
 
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+    this.activePage = page;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,105 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { OneSignal } from '@ionic-native/onesignal';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
+//import { HomePage } from '../pages/home/home';
+//import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { TermsPage } from '../pages/terms/terms';
+import { CreateAccountPage } from '../pages/create-account/create-account';
+import { JoinNowPage } from '../pages/join-now/join-now';
+import { MembershipOptionsPage } from '../pages/membership-options/membership-options';
+import { WeatherPage } from '../pages/weather/weather';
+import { WebcamPage } from '../pages/webcam/webcam';
+import { CamviewPage } from '../pages/camview/camview';
+import { CumulativeSnowfallPage } from '../pages/cumulative-snowfall/cumulative-snowfall';
+import { NavigationPage } from '../pages/navigation/navigation-component';
+import { AirportPage } from '../pages/airport/airport';
+import { HeaderComponent } from '../pages/header/header.component';
+import { FooterComponent } from '../pages/footer/footer.component';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
+import { AppServiceProvider } from '../providers/app-service/app-service';
+import { HttpModule } from '@angular/http';
+import {ChartsModule} from 'ng2-charts/charts/charts';
+import {IonicStorageModule} from '@ionic/storage';
+import { cardPage } from '../pages/card/card';
+import '../../node_modules/chart.js/dist/Chart.bundle.min.js';
+import { Stripe } from '@ionic-native/stripe';
+import { accountPage } from '../pages/account/account';//footerPage
+import { aboutUSPage } from '../pages/aboutUS/aboutUS';//
+import { footerPage } from '../pages/footer/footer';//
+import { DatePipe } from '@angular/common'
+import { AlertsPage } from '../pages/alerts/alerts';
+import {TermPage } from '../pages/term/term';
+import { PrivacypolicyPage } from '../pages/privacypolicy/privacypolicy';
+import { InAppPurchase } from '@ionic-native/in-app-purchase';
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    LoginPage,
+    CreateAccountPage,
+    JoinNowPage,
+    MembershipOptionsPage,
+    WeatherPage,
+    WebcamPage,
+    CamviewPage,
+    aboutUSPage,
+    NavigationPage,
+    AirportPage,
+    CumulativeSnowfallPage,
+    HeaderComponent,
+    cardPage,
+    accountPage,
+    FooterComponent,
+    AlertsPage,
+    TermsPage,
+    TermPage,
+    PrivacypolicyPage,
+
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    ChartsModule,
     IonicModule.forRoot(MyApp),
+     IonicStorageModule.forRoot()
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage
+        MyApp,
+        LoginPage,
+        CreateAccountPage,
+        JoinNowPage,
+        MembershipOptionsPage,
+        WeatherPage,
+        WebcamPage,
+        CamviewPage,
+        NavigationPage,
+        aboutUSPage,
+        AirportPage,
+        cardPage,
+        accountPage,
+        CumulativeSnowfallPage,
+        HeaderComponent,
+        FooterComponent,
+        AlertsPage,
+        TermsPage,
+        TermPage,
+        PrivacypolicyPage,
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Stripe,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AppServiceProvider,
+    OneSignal,
+    DatePipe,
+    InAppPurchase
   ]
 })
 export class AppModule {}
